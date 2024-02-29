e37a835ca1b558d0fb7e1546503adf4c302c475b
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-02-29 21:52:56 +0100

    Add code to remove given values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  cleanedArray = [];
 
+  array.forEach((element) => { 
+    if (!remove.includes(element)) {
+      cleanedArray.push(element);
+    }
+  })
+
+  return cleanedArray;
 };
 
 // Do not edit below this line
